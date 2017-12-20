6f0b5271025e8b0bf668c29fa1f0249dedad18e7
Alix Grados <alixgrados@gmail.com>
2017-12-20 17:30:35 +0100

    Initial version

---
+++ app.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+var mongodb = require('mongodb');
+var client = mongodb.MongoClient;
+
+var uri = "mongodb://mongo/dummy-app";
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/entry', function(req, res, next) {
+    client.connect(uri, function (err, db) {
+	    if (err) return next(err);
+    	var collection = db.collection('entry');
+    	collection.find({}).toArray(function(err, docs) {
+			if (err) return next(err);
+			return res.json(docs);
+    	});
+	});
+});
+
+router.post('/entry', function(req, res, next) {
+	client.connect(uri, function (err, db) {
+	    if (err) return next(err);
+    	var collection = db.collection('entry');
+    	collection.insertMany(req.body, function(err, result) {
+			return res.json({ result: "success" });
+    	});
+	});
+});
+
+module.exports = router;
